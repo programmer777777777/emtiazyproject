1cba15d28b0c2a10d8c879eb71eee2c0c61a296f
Waheeb <waheeb10011@gmail.com>
2022-01-07 10:43:15 -0800

    Fix some bugs

--- emtiazyproject/settings.py
+++ emtiazyproject/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fq0xhnb254%$_qo9kbro^(=s!g(^*%+s3rn%^08yk))-(!!wwz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'http://147.182.133.202/']
 
 # Application definition
 
@@ -141,7 +141,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 's/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
