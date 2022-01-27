d9aed3f53665bf06d63df4e55b11a9367dd08e75
Waheeb <waheeb10011@gmail.com>
2022-01-27 08:14:57 -0800

    commit

--- emtiazyproject/settings.py
+++ emtiazyproject/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-fq0xhnb254%$_qo9kbro^(=s!g(^*%+s3rn%^08yk))-(!!wwz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost',]
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '147.182.133.202', 'www.emtiazy.com', 'emtiazy.com']
 
 # Application definition
 
@@ -141,7 +141,7 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 's/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'build/static'),
